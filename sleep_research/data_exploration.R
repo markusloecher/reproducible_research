6852b88ff23c18e87f2a4c5dfe57d9119390d5dd
raphaef98 <64739712+raphaef98@users.noreply.github.com>
2023-02-27 08:54:14 +0100

    code from discussion

--- sleep_research/data_exploration.R
+++ sleep_research/data_exploration.R
@@ -2,52 +2,73 @@ library("readxl")
 library("dplyr")
 library("ggplot2")
 library("grid")
+library("emmeans")
 
-data <- read_xlsx("journal.pbio.3001733.s008.xlsx", sheet = "S3_Data_Fig3B")
+data <- read_xlsx("journal.pbio.3001733.s008.xlsx", sheet = "S3_Data_FigS2")
 
-#Data between 2001 and 2016
-min(data$donation_date)
-max(data$donation_date)
-
-#approx 250 for pre,post,and DST. 2200 regular
-table(data$Type_of_week)
-
-#Fig 3b. Alle means zu hoch
-data %>% group_by(Type_of_week) %>% summarize(mean = log(mean(daily_donations_mean)))
-
-
-#sieht nicht schlecht aus, aber der Tail ist zu heavy. Ich glaube der density plot wurde mit den 
-#Daten auf Individual-Level gemacht, nicht mit daily averages (siehe Dichte unter 1)
+#Density plot sieht gut aus, wobei ich immer noch etwas verwirrt bin, dass im Original Masse unter 1 liegt
 ggplot(data,aes(x = log(daily_donations_mean))) + geom_density(fill = "lightgreen")
 
-#mean ist komplett daneben 122 statt gewünschten 82. Fehlt hier das Processing auf den Daten?
-mean(data$daily_donations_mean)
-#3*sd abschneiden auf den normalen Daten reicht nicht ganz, aber sieht näher am Original aus. 
-#Das hat ja aber nichts mit den 100k per individual observation zu tun, die genannt wurden
-cut = mean(data$daily_donations_mean) + 3*sd(data$daily_donations_mean)
 
-#Intuitives outlier removal verändert den mean kaum, nimmt aber 75 Datenpunkte raus.
-mean(data$daily_donations_mean[-which(data$daily_donations_mean > cut)])
-#Das sind ungefähr die 2%, die im Paper angegeben werden
-sum(data$daily_donations_mean > cut)/nrow(data)
+data2 <- read_xlsx("journal.pbio.3001733.s008.xlsx", sheet = "S3_Data_Fig3B")
+#mean ist komplett daneben 122 statt gewünschten 82. Processing sei allerdings schon passiert
+mean(data2$daily_donations_mean)
 
-#Unintuitives outlier removal: mean berechnen nach der Log-transformation. Ungewöhlich, aber führt zu Ergebnissen, die näher am Original sind
-exp(mean(log(data$daily_donations_mean[-which(data$daily_donations_mean > cut)])))
+lm3 <- glm(log(daily_donations_mean) ~ Type_of_week+Time_of_Day+ Weekend_day+ YEAR +as.factor(MONTH)+as.factor(DAY_OF_WEEK)+Holiday, family="gaussian", data=data2)
+#Damit kriegen wir tatsächlich die korrekten marginal means. Auch die Signifikanz passt
+emmeans(lm3, ~ Type_of_week)
+summary(lm3)
+
+lm1 <- lm(log(daily_donations_mean) ~ Type_of_week+Weekend_day + YEAR +as.factor(MONTH)+as.factor(DAY_OF_WEEK)+Holiday, data=data2)
+#r-sq 0.3719 (ganzes model) oder 0.36 (ohne time_of_day)
+summary(lm1)
 
 
 ###Plot C
-#Wurde mit diesen Daten outlier removal durchgeführt? Muss das noch passieren? 
+#Mit selber Strategie wie zuvor kommen wir hier voran. Emmeans berechnen und die CIs davon nutzen. Das sieht gut aus
 data_c1 <- read_excel("journal.pbio.3001733.s008.xlsx", sheet = "S3_Data_Fig3C_top")
 data_c2 <- read_excel("journal.pbio.3001733.s008.xlsx", sheet = "S3_Data_Fig3C_bottom")
 #Verschieben um die relevante Woche, damit es um 0 zentriert ist
 w_dst = data_c1[data_c1$Type_of_week == "DST_Week",]$WEEK[1]
 w_st = data_c2[data_c2$Type_of_week == "ST_Week",]$WEEK[1]
 
-p1 <- data_c1 %>% group_by(WEEK) %>% summarize(mean = log(mean(daily_donations_mean))) %>% 
-  ggplot(aes(x = WEEK-w_dst,y = mean)) + geom_line()
-p2 <- data_c2 %>% group_by(WEEK) %>% summarize(mean = log(mean(daily_donations_mean))) %>% 
-  ggplot(aes(x = WEEK-w_st,y = mean)) + geom_line()
+lm4 <- glm(log(daily_donations_mean) ~ as.factor(WEEK), family="gaussian", data=data_c1)
+df_3c_up <- summary(emmeans(lm4, ~ WEEK))
+lm5 <- glm(log(daily_donations_mean) ~ as.factor(WEEK), family="gaussian", data=data_c2)
+df_3c_down <- summary(emmeans(lm5, ~ WEEK))
 
-#Auch in Plot c) sind alle Werte zu hoch. 
+data_c1_transf <- data_c1 %>% mutate(weektype = WEEK == w_dst)
+lm6 <- glm(log(daily_donations_mean) ~ weektype, family="gaussian", data=data_c1_transf)
+summary(lm6)
+data_c2_transf <- data_c2 %>% mutate(weektype = WEEK == w_st)
+lm7 <- glm(log(daily_donations_mean) ~ weektype, family="gaussian", data=data_c2_transf)
+summary(lm7)
+
+p1 <- df_3c_up %>% 
+  ggplot(aes(x = WEEK-w_dst,y = emmean)) + geom_line() +  
+  geom_ribbon(aes(ymin = emmean, ymax = upper.CL), alpha = 0.1) +
+  ylim(4.3,4.7)
+p2 <- df_3c_down %>% 
+  ggplot(aes(x = WEEK-w_st,y = emmean)) + geom_line() +  
+  geom_ribbon(aes(ymin = emmean, ymax = upper.CL), alpha = 0.1) + 
+  ylim(4.15,4.55)
 grid.newpage()
 grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), size = "last"))
+
+
+#'p2 <- data_c2 %>% group_by(WEEK) %>% summarize(mean = log(mean(daily_donations_mean))) %>% 
+#'  ggplot(aes(x = WEEK-w_st,y = mean)) + geom_line() + geom_ribbon(aes(ymin = low, ymax = high), alpha = 0.1)
+
+library(lubridate)
+temp <- data %>% mutate(dayofyear = yday(donation_date))
+
+temp2 <- aggregate(log(daily_donations_mean)~dayofyear,FUN = mean,data = temp)
+plot(as.Date("2001-01-01")+temp2$dayofyear,temp2$`log(daily_donations_mean)`,type="l")
+abline(v = as.Date("2001-03-15"),col = "red")
+abline(v = as.Date("2001-11-15"),col = "blue")
+
+x = as.Date("2001-01-01")
+
+temp3 <- data %>% mutate(year = year(donation_date))
+temp3 <- aggregate(log(daily_donations_mean)~year,FUN = mean,data = temp3)
+plot(temp3$year,temp3$`log(daily_donations_mean)`,type="l")
